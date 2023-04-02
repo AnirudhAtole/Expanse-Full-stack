50eb76b9f44e2171775d809ac0741a5ac4cf0ff8
AnirudhAtole <anirudhatole@gmail.com>
2023-04-02 18:13:21 +0530

    optimized sequelize

--- controller/premium.js
+++ controller/premium.js
@@ -1,30 +1,24 @@
 const Expanse = require('../models/Expanse');
 const User = require('../models/User');
+const sequelize = require('../utils/database')
 
 exports.leaderBoard = async (req,res) =>{
-    const userList = []
-   const result = await User.findAll();
-   for(let user of result){
-    const userId = user.dataValues.userId;
-    const expanses = await Expanse.findAll({
-        where:{
-            UserUserId : userId
-        }
-    })
-    let sum = 0;
-    for(let expanse of expanses){
-        sum += expanse.dataValues.amount;
+    try{
+        const userList = await User.findAll(
+            {
+                attributes : ['userName',[sequelize.fn('sum' , sequelize.col('expanse-lists.amount')) , 'totalAmount']],
+                include : [{
+                    model : Expanse,
+                    attributes : []
+                }],
+                group : ['userId'],
+                order : [['totalAmount' , 'DESC']]
+    
+            }
+        )
+       res.json(userList)
     }
-    userList.push({user:user.dataValues.userName , userSum : sum})
-   }
-   for(let i = 0 ; i < userList.length; i++){
-    for(let j = 0 ; j < userList.length -1 ; j++){
-        if(userList[j].userSum < userList[j+1].userSum){
-            const temp = userList[j];
-            userList[j] = userList[j+1];
-            userList[j+1] = temp
-        }
+    catch(err){
+        console.log(err);
     }
-   }       
-   res.json(userList)
 }
